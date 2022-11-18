cb00d8902f88aa84d484bfd359747af502f7dc7e
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-18 15:03:59 +0100

    added job image

--- src/pages/PageDashboard.tsx
+++ src/pages/PageDashboard.tsx
@@ -4,7 +4,10 @@ export const PageDashboard = () => {
 	return (
 		<div className="page pageDashboard">
 			<p>This is the Dashboard page.</p>
-			<img className="employment" src={employment}/>
+			<img className="employment" src={employment} />
+			<div>
+				<img src="images/job.jpg" />
+			</div>
 		</div>
 	);
 };
